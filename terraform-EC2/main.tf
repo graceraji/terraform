d39800331a3eec2dde906efb3ee5d3af5e62040f
graceraji <144625483+graceraji@users.noreply.github.com>
2023-11-01 15:06:45 -0400

    Update main.tf

--- terraform-EC2/main.tf
+++ terraform-EC2/main.tf
@@ -1,1 +1,30 @@
+terraform {
 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_ecs_cluster" "tf-cluster" {
+  name = var.ecs_cluster_name
+}
+
+resource "aws_ecs_cluster_capacity_providers" "tf-Fargate" {
+  cluster_name = aws_ecs_cluster.tf-cluster.name
+
+  capacity_providers = ["FARGATE"]
+
+  default_capacity_provider_strategy {
+    base              = 1
+    weight            = 10
+    capacity_provider = "FARGATE"
+  }
+}
