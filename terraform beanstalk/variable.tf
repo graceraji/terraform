13600114f0012adaf74912e290b282e790112976
graceraji <144625483+graceraji@users.noreply.github.com>
2023-11-01 15:14:21 -0400

    Update variable.tf

--- terraform beanstalk/variable.tf
+++ terraform beanstalk/variable.tf
@@ -1,1 +1,24 @@
+variable "aws_region" {
+  description = "The AWS region where resources will be created"
+  type        = string
+}
 
+variable "app_name" {
+  description = "Name of the Elastic Beanstalk application"
+  type        = string
+}
+
+variable "env_name" {
+  description = "Name of the Elastic Beanstalk environment"
+  type        = string
+}
+
+variable "solution_stack" {
+  description = "Name of the solution stack"
+  type        = string
+}
+
+variable "instance_profile" {
+  description = "Name of the IAM instance profile"
+  type        = string
+}
