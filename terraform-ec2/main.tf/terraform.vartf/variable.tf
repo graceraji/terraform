705b5c3390ea79ea14f69abe4e7ab4b77538f3fd
graceraji <144625483+graceraji@users.noreply.github.com>
2023-11-01 15:03:19 -0400

    Delete terraform-ec2/main.tf/terraform.vartf directory

--- terraform-ec2/main.tf/terraform.vartf/variable.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_region" {
-  description = "The AWS region where resources will be created"
-  type        = string
-}
-
-variable "ecs_cluster_name" {
-  description = "Name of the ECS cluster"
-  type        = string
-}
